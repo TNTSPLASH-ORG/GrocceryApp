7a70886ce2617c5cd9d97db3fda3d9535030fd0f
BatDev <brodycritchlow@gmail.com>
2020-01-31 09:30:51 -0700

    Added the start to the Google Sheets .net Api
    I added all the using parts, and added a namespace tag into my c# file.

--- Scanner/GetGoogleSheet.cs
+++ Scanner/GetGoogleSheet.cs
@@ -1,9 +1,20 @@
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
 using System;
-					
-public class Program
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace SheetsApi 
 {
-	public static void Main()
+	public class Program
 	{
+		public static void Main()
+		{
 		Console.WriteLine("Hello World");
+		}
 	}
 }
